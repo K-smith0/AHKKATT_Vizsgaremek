1b8ea62e83a9276bf98739a1e4ac5deab5596792
HunorAbraham <hunor335@gmail.com>
2024-10-24 08:41:43 +0200

    path interpretation works

--- website/index.php
+++ website/index.php
@@ -2,15 +2,18 @@
 <html>
 <?php
 	$path = parse_url($_SERVER['REQUEST_URI'])["path"];
-	//echo($path.'<br>');
-	switch($path){
-		case "./":
+	$pathArray = explode('/',$path);
+	$subPath = end($pathArray);
+	//echo($subPath .'<br>');
+	
+	switch($subPath){
+		default:
 			echo "<a href='./signUp' target='_self'>Sign up</a>";
 			break;
-		case "./signUp":
+		case "signUp":
 			require_once("SignUp.html");
 			break;
-		case "./signUpResolve":
+		case "signUpResolve":
 			//(...)
 			require_once("SignUpResolve.php");
 			break;
