c467ad0c6cb2240866c4819b3ec574a55ad90a18
K-Smith0 <122173ks@gmail.com>
2024-11-11 08:42:13 +0100

    Gedva

---
+++ website/TitleTemplate.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="Public/CSS/style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><!-- TITLE --></title>
+</head>
+<body>
+    <div class="backgroundbox">
+		<div class="background"></div>
+		<form class="centerbox">
+            <!-- IDE ÍRJ MINDENT -->
+            <button type="submit"><!-- SUBMIT BUTTON!!!!!! --></button>
+            <p><a href="./">Return to Home page</a></p>
+        </form>	
+	</div>
+</body>
+</html>
