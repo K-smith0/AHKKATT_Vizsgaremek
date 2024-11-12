e9ba1a37508908b3bfa29970056ccc81ab01bbb6
HunorAbraham <hunor335@gmail.com>
2024-11-12 13:10:40 +0100

    restructure revert 1, js, html

--- website/Public/JS/mainPage.js
+++ website/Public/JS/mainPage.js
@@ -10,36 +10,8 @@ req.onreadystatechange = ()=>{
 };
 req.send();
 
-function load(target){
-    console.log(links,target);
-    req = new XMLHttpRequest();
-    req.open("GET",links[target]);
-    req.onreadystatechange = () =>{
-        if(req.status === 200 && req.readyState === 4){
-            console.log(req.responseText);
-            document.getElementById("frame").innerHTML = req.responseText;
-        }
-    }
-    req.send();
-
-}
-
 
 
 function start(){
-    req = new XMLHttpRequest();
-    req.open("GET",links["home"]);
-    req.onreadystatechange = () =>{
-        if(req.status === 200 && req.readyState === 4){
-            console.log(req.responseText);
-            document.getElementById("frame").innerHTML = req.responseText;
-            document.getElementById("signUp").onclick = ()=>{
-                load("signUp");
-            };
-            document.getElementById("signIn").onclick = ()=>{
-                load("signIn");
-            };
-        }
-    }
-    req.send();
+    
 }
