21a29d0a754dcfe01a9a70fb97403d74acc1fdcc
HunorAbraham <hunor335@gmail.com>
2024-11-14 09:48:24 +0100

    zoom partially works

---
+++ website/Public/JS/magic.js
@@ -0,0 +1,31 @@
+console.log("here");
+let bod = document.querySelectorAll("body")[0];
+let svg = document.querySelectorAll("svg")[0];
+let cont = document.getElementById("contain");
+const maxScale = 20;
+let currentScale = 1;
+let isMouseDown = false;
+bod.onmousemove=(e)=>{
+    if(isMouseDown){
+        window.scrollBy(-e.movementX,-e.movementY);
+    }
+}
+bod.onmousedown=()=>isMouseDown=true;
+bod.onmouseup=()=>isMouseDown=false;
+
+bod.onwheel=(e)=>{
+    if(e.deltaY<0){
+        currentScale = Math.min(maxScale, currentScale*1.1);
+    }
+    else{
+        currentScale = Math.max(1,currentScale/1.1);
+    }
+    svg.style.transform = `scale(${currentScale})`;
+    cont.style.transform = `translate(${svg.width.baseVal.value*(currentScale-1)/2}px,${svg.height.baseVal.value*(currentScale-1)/2}px)`;
+    svg.style.strokeWidth = 1/currentScale;
+}
+document.querySelectorAll("path").forEach((x)=>{
+    x.onclick=()=>{
+        console.log(x.id);
+    };
+});
