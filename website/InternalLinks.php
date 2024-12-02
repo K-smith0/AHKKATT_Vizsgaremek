513dd4b3d115ba41514a6b115b25f37cae39bc3c
HunorAbraham <hunor335@gmail.com>
2024-12-02 09:41:42 +0100

    clean up

--- website/InternalLinks.php
+++
@@ -1,5 +0,0 @@
-<?php
-header("Content-Type: application/json");
-$links = array("home"=>"./public/pages/home.html","signUp"=>"./public/pages/signUp.html","signIn"=>"./public/pages/signIn.html");
-echo json_encode($links);
-?>
